625a49ba55303519776fb60bc89aa5f14f25a138
Marco Sanchez-Ayala <sanchezayala.marco@gmail.com>
2020-05-26 23:51:00 -0400

    finish implementation

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -1,7 +1,8 @@
 """
 A stack is a FIFO (first in first out) data structure. In contrast to a stack, the first element in is also the first one out now.
 """
-from .node import node
+from .node import Node
+from .stack import SizeError
 
 class Queue:
     def __init__(self, node = None):
@@ -10,14 +11,43 @@ class Queue:
         ----------
         node: [Node or None] the node to be first inserted into the queue
         """
-        self.top = node
-        self.bottom = node
+        self.rear = node
+        self.front = node
 
     def enqueue(self, value):
-        pass
+        """
+        Adds a new rear node and shifts others down 1.
+
+        Parameters
+        ----------
+        value: [number or str] The value of the node to be added
+        """
+        if self.front == None:
+            node = Node(value)
+            self.front = node
+            self.rear = node
+        
+        else:
+            old_rear = self.rear
+            new_rear = Node(value)
+            old_rear.next = new_rear
+            self.rear = new_rear
 
     def dequeue(self):
-        pass
+        """
+        Returns
+        -------
+        The value of the front node. Also removes that node from the queue.
+        """
+        old_front = self.front
+        new_front = old_front.next
+        self.front = new_front
+        return old_front.value
 
     def peek(self):
-        pass
+        """
+        Returns
+        -------
+        The value of the front node WITHOUT removing that node from the queue.
+        """
+        return self.front.value
--- tests/test_queue.py
+++ tests/test_queue.py
@@ -2,21 +2,66 @@ import pytest
 from data_structures.queue import Queue
 from data_structures.node import Node
 
-class testQueue:
+class TestQueue:
     def test_init(self):
         queue = Queue()
-        assert queue.top == None, 'Top wrongfully assigned a value'
-        assert queue.bottom == None, 'Bottom wrongfully assigned a value'
+        assert queue.rear == None, 'Rear wrongfully assigned a value'
+        assert queue.front == None, 'Front wrongfully assigned a value'
 
         queue = Queue(Node(1))
-        assert queue.bottom.value == 1\
-            , 'Bottom node value not assigned correctly'
-
+        assert queue.rear.value == 1\
+            , 'Rear node value not assigned correctly'
+        assert queue.front.value == 1\
+            , 'Front node value not assigned correctly'
+        assert queue.front.next == None\
+            , 'Front node has a reference to another when it should not.'
+        
     def test_enqueue(self):
-        pass
+        queue = Queue()
+        queue.enqueue(1)
+        assert queue.rear.value == 1\
+            , 'Rear node value not enqueued correctly'
+        assert queue.front.value == 1\
+            , 'Front node value not enqueued correctly'
 
+        queue.enqueue(2)
+        assert queue.rear.value == 2\
+            , 'Rear node value not enqueued correctly'
+        assert queue.front.value == 1\
+            , 'Front node value was incorrectly affected'
+        assert queue.front.next == queue.rear\
+            , 'Proper relationship between nodes not established.'
+    
     def test_dequeue(self):
-        pass
+        queue = Queue()
+        front = Node(1)
+        middle = Node(2)
+        rear = Node(3)
+
+        queue.front = front
+        queue.front.next = middle
+        middle.next = rear
+        queue.rear = rear
+
+        assert queue.dequeue() == 1\
+            , 'Returned value is not the front node\'s value'
+        assert queue.front == middle\
+            , 'Front node was not removed or referencing is incorrect'
+        assert queue.front.next == queue.rear\
+            , 'Node referencing was incorrectly affected'
 
     def test_peek(self):
-        pass
+        queue = Queue()
+        front = Node(1)
+        rear = Node(2)
+
+        queue.front = front
+        queue.front.next = rear
+        queue.rear = rear
+
+        assert queue.peek() == 1\
+            , 'Not returning the top node\'s value'
+        assert queue.front == front\
+            , 'Peek affected the node structure when it shouldn\'t have'
+        assert queue.rear == rear\
+            , 'Non-front nodes affected by peek'
